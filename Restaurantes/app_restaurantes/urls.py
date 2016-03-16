cc6f185b2a3775e3c785083c8053e30eb8c8eebf
JCristobal <tobas92@gmail.com>
2016-03-16 13:03:29 +0100

    Leve mejora en el aspecto a la hora de fallar en logue y registro. #2 También se añade confirmación a la hora de registro. Y se redirige a index si se deja dirección en blanco.

--- Restaurantes/app_restaurantes/urls.py
+++ Restaurantes/app_restaurantes/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # para /app_restaurante/index
     url(r'^index$', views.index, name='index'),
-    # para /app_restaurante/otro_url
+    # para /app_restaurante/hola
     url(r'^hola$', views.hola_funcion, name='hola_funcion'),
+    # para /app_restaurante/ 
+    url(r'^$', views.index, name='index'),
 
 ]
 
