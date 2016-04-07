f85cc36a5823817d1615fa53c710c7a1eb608087
JCristobal <tobas92@gmail.com>
2016-04-07 18:59:58 +0200

    Completo con views.py el commit anterior (https://github.com/JCristobal/SSBW-gestion-restaurantes/commit/eb0835dc307ae3e4cd86834ec63b0be865d548d1) y botones 'me gusta' y 'no me gusta' modificados #3

--- Restaurantes/app_restaurantes/views.py
+++ Restaurantes/app_restaurantes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from app_restaurantes.models import Restaurante
+from django.contrib.auth.decorators import login_required
+
 
 def hola_funcion (request):
     return HttpResponse("Hola mundo")
@@ -10,3 +12,40 @@ def index (request):
 	lista = Restaurante.objects.all()
 	context= {'mensaje': 'restaurantes!', 'restaurante': lista[:4]} #paso los 4 primeros solo
 	return render (request, 'base.html', context)
+
+
+@login_required
+def like_category(request):
+	cat_id = None
+	if request.method == 'GET':
+        	cat_id = request.GET.get('me_gusta','')	
+	likes = 0
+	if cat_id:
+		cat = Restaurante.objects.get(id=cat_id)
+	if cat:
+		likes = cat.me_gusta + 1
+		cat.me_gusta =  likes
+		cat.save()
+
+
+
+	return HttpResponse(likes)
+
+
+@login_required
+def dislike_category(request):
+	cat_id = None
+	if request.method == 'GET':
+        	cat_id = request.GET.get('no_me_gusta','')
+	dislikes = 0
+	if cat_id:
+		cat = Restaurante.objects.get(id=cat_id)
+	if cat:
+		dislikes = cat.no_me_gusta + 1
+		cat.no_me_gusta =  dislikes
+		cat.save()
+
+
+
+	return HttpResponse(dislikes)
+
